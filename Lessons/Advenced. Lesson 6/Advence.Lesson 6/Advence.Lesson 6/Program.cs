9448825175666baa81dd9d0bcd5e2eb382b2a210
Olga R <rolya85@bk.ru>
2018-10-07 23:55:45 +0300

    Lesson 6 Practice

--- Lessons/Advenced. Lesson 6/Advence.Lesson 6/Advence.Lesson 6/Program.cs
+++ Lessons/Advenced. Lesson 6/Advence.Lesson 6/Advence.Lesson 6/Program.cs
@@ -11,7 +11,12 @@ namespace Advence.Lesson_6
     {
         static void Main(string[] args)
         {
-            Practice1();
+            //SysteIOUsageExample();
+            //WriteToFileWithStream();
+            //Practice1();
+            //ReadFromFileWithStream();
+            //WriteToFileWithStream();
+            Practice2();
         }        
 
         private static void SysteIOUsageExample()
@@ -22,6 +27,60 @@ namespace Advence.Lesson_6
 
             Directory.Delete("d://temp");
             File.Open("c://pagefile.sys", FileMode.Open);
+        }
+
+        private static void WriteToFileWithStream()
+        {
+            /*FileInfo file = new FileInfo(@"d:\File.txt");
+            FileStream fs = file.Open(FileMode.OpenOrCreate, FileAccess.ReadWrite);
+
+            StreamWriter sw = new StreamWriter(fs, Encoding.Default);
+            sw.Write("Hello World");
+            sw.Close(); */ 
+            
+            StreamWriter sw = new StreamWriter(@"d:\File.txt");
+            sw.Write("Hello World");
+            sw.Close();         
+        }
+
+        private static void WriteToFileWithStreamWriter()
+        {
+            FileInfo file = new FileInfo(@"d:\File.txt");
+            FileStream fs = file.Open(FileMode.OpenOrCreate, FileAccess.ReadWrite);
+
+            byte[] word = Encoding.Default.GetBytes("Hello World");
+            fs.Write(word, 0, word.Length);
+
+            fs.Close();
+        }
+
+        private static void ReadFromFileWithStream()
+        {
+            FileInfo file = new FileInfo(@"d:\File.txt");
+            FileStream fs = file.Open(FileMode.OpenOrCreate, FileAccess.ReadWrite);
+
+            byte[] word = new byte[fs.Length];
+            fs.Read(word, 0, (int)fs.Length);
+            Console.Write(Encoding.Default.GetChars(word));    
+            fs.Close();
+        }
+
+        private static void Practice2()
+        {
+            var iteration = 0;
+
+            using (StreamWriter sw = new StreamWriter(@"d:\File.txt"))
+            {
+                while (iteration++ < 2)
+                {
+                    for (int i = 1; i <= 2; i++)
+                    {
+                        Console.Write($"User {i}: ");
+                        sw.Write($"User {i} [{DateTime.Now.ToString()}]: {Console.ReadLine()}");
+                        sw.WriteLine();
+                    }                    
+                }
+            }
         }
 
         private static void Practice1()
