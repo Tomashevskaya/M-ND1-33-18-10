6d77835248ec0055d28f08c9141a553ba4b6eb9a
Olga R <rolya85@bk.ru>
2018-10-12 19:27:21 +0300

    Letters update

--- Lessons/Base. Lesson 7/Base. Lesson 7/Base. Lesson 7/Program.cs
+++ Lessons/Base. Lesson 7/Base. Lesson 7/Base. Lesson 7/Program.cs
@@ -12,7 +12,7 @@ namespace Base.Lesson_7
         {
             //Sturcture();
             LettersExample();
-
+            //Sturcture();
             Console.ReadKey();
         }
 
@@ -27,35 +27,55 @@ namespace Base.Lesson_7
 
         public static void LettersExample()
         {
-            var letterTemplate = new Letter(new char[4, 4]{
+            var letterTemplate = new Letter(' ', new char[4, 4]{
                 { ' ', ' ', ' ', ' ' },
                 { ' ', ' ', ' ', ' ' },
                 { ' ', ' ', ' ', ' ' },
                 { ' ', ' ', ' ', ' ' }
             });
 
-            var o = new Letter(new char[4, 4]{
+            var o = new Letter('о', new char[4, 4]{
                 { ' ', ' ', ' ', ' ' },
                 { ' ', '/', '\\', ' ' },
                 { '|', ' ', '|', ' ' },
                 { ' ', '\\', '/', ' ' }
             });
 
-            var l = new Letter(new char[4, 4]{
+            var l = new Letter('л', new char[4, 4]{
                 { ' ', '_', '_', ' ' },
                 { ' ','|', ' ', '|' },
                 { ' ','|', ' ', '|' },
                 { '/',' ', ' ', '|' }
             });
 
-            var ya = new Letter(new char[4, 4]{
+            var ya = new Letter('я', new char[4, 4]{
                 { ' ', '_', '_', ' ' },
                 { ' ','|', ' ', '|' },
                 { ' ','|', '_', '|' },
                 { ' ',' ', '/', '|' }
             });
 
-            writeLetters(o, l, ya);            
+            Letter[] alphebet = new Letter[]
+            {
+                o, l, ya
+            };
+
+            var word = Console.ReadLine();
+            var pseudoLetters = new List<Letter>();
+
+            for (int i = 0; i < word.Length; i++)
+            {
+                var letter = word[i];
+                for (int j = 0; j < alphebet.Length; j++)
+                {
+                    if (alphebet[j].letterName == letter)
+                    {
+                        pseudoLetters.Add(alphebet[j]);
+                    }
+                }
+            }
+
+            writeLetters(pseudoLetters.ToArray());            
         }
 
         public static void writeLetters(params Letter[] word)
@@ -80,11 +100,12 @@ namespace Base.Lesson_7
     struct Letter
     {
         public char[,] graphics;
+        public char letterName;
 
-
-        public Letter(char[,] graphics)
+        public Letter(char letterName, char[,] graphics)
         {
             this.graphics = graphics;
+            this.letterName = letterName;
         }
     }
 
